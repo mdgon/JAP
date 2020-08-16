d5fa99381ccfd43b17d9c3bf05ed486e875dc2ae
mdgon <55919504+mdgon@users.noreply.github.com>
2020-08-16 17:24:36 -0300

    Se agrega boton de google al login

--- js/login.js
+++ js/login.js
@@ -5,6 +5,16 @@ function ingresarHomePage(){
 localStorage.setItem("boolean", true);
 }
 
+
+function onSignIn2(googleUser) {
+var profile = googleUser.getBasicProfile();
+console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+console.log('Name: ' + profile.getName());
+console.log('Image URL: ' + profile.getImageUrl());
+console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+localStorage.setItem("boolean", true);
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
